23d8c481856a4376a0f6b74642bbec6612dffa22
Mohamed El tamawey <tammwy22@gmail.com>
2025-05-12 22:15:01 +0300

    refactor: clean up Ball class constructor and methods for improved readability

--- game_objects/ball.py
+++ game_objects/ball.py
@@ -1,5 +1,5 @@
 from typing import Tuple
-from game_engine import  SCREEN_WIDTH, SCREEN_HEIGHT
+from game_engine import SCREEN_WIDTH, SCREEN_HEIGHT
 
 from game_objects.player_bar import PlayerBar
 import pygame
@@ -7,7 +7,9 @@ import pygame
 
 class Ball:
 
-    def __init__(self, init_speed: Tuple[float, float], speed_inc: float ,context) -> None:
+    def __init__(
+        self, init_speed: Tuple[float, float], speed_inc: float, context
+    ) -> None:
         self._x = PlayerBar.BAR_WIDTH + 50
         self._y = 30
         self._init_speed = init_speed
@@ -15,14 +17,11 @@ class Ball:
         self._speed_inc = speed_inc
         self._context = context
 
-
-
     def _collide(self, on_X: bool):
         """
         Collide with a wall, change the direction of the Ball
         :param on_X: True if the ball collides with a wall on the X axis
         :return: None
-
         """
 
         self._speed = (
@@ -44,7 +43,7 @@ class Ball:
 
         self._speed = (sign[0] * self._speed[0], sign[1] * self._speed[1])
 
-    def move(self, delta_time:float, player_bar: PlayerBar):
+    def move(self, delta_time: float, player_bar: PlayerBar):
         """
         Move the ball by the speed value
         :return: None
@@ -52,12 +51,12 @@ class Ball:
         self._x += self._speed[0]
         self._y += self._speed[1]
         if (
-            self._x <= PlayerBar.BAR_WIDTH and
-            player_bar.y <= self._y <= player_bar.y + player_bar.BAR_HEIGHT
+            self._x <= PlayerBar.BAR_WIDTH
+            and player_bar.y <= self._y <= player_bar.y + player_bar.BAR_HEIGHT
         ):
             self._x = PlayerBar.BAR_WIDTH + 1
             self._collide(True)
-            self._context['counter'] +=1
+            self._context["counter"] += 1
             self._increase_speed()
 
         elif self._x >= SCREEN_WIDTH:
@@ -84,13 +83,14 @@ class Ball:
         """
         return self._x < PlayerBar.BAR_WIDTH
 
-    def reset(self):
+    def reset(self, speed: Tuple[float, float] | None =None, speed_inc: float| None= None):
         """
         Reset the ball position
         """
+        if  speed:
+            self._init_speed = speed
+        if speed_inc:
+            self._speed_inc = speed_inc
         self._x = PlayerBar.BAR_WIDTH + 50
         self._y = 30
         self._speed = self._init_speed
-        print(self._init_speed , self._speed)
-
-
